a88d079aeb2d241708645eb705edcaabe86ff4c1
camillawilliams <camillawilliamsdu@gmail.com>
2020-12-11 02:43:40 +0000

    function that returns

--- exercises/04-Call-a-function/app.js
+++ exercises/04-Call-a-function/app.js
@@ -3,9 +3,16 @@ function calculateArea(length,edge)
 	return length * edge;
 }
 // Your code Below this line:
+// var functionCall = calculateArea
+
+let squareArea1 = calculateArea(4,4)
+let squareArea2 = calculateArea(2,2)
+let squareArea3 = calculateArea(5,5)
+
+var calculateArea = calculateArea
 
 
 
-
-
-
+console.log(squareArea1);
+console.log(squareArea2);
+console.log(squareArea3);
